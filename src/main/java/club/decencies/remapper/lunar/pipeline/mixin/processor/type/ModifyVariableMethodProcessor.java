6445601bacdcc35d9532887ac2a0b2ca785549b3
Decencies <66835910+Decencies@users.noreply.github.com>
2022-01-25 16:07:20 +0000

    Temp-Fix: Removed exception.

--- src/main/java/club/decencies/remapper/lunar/pipeline/mixin/processor/type/ModifyVariableMethodProcessor.java
+++ src/main/java/club/decencies/remapper/lunar/pipeline/mixin/processor/type/ModifyVariableMethodProcessor.java
@@ -15,7 +15,7 @@ public class ModifyVariableMethodProcessor extends MethodProcessor {
     public void process(MethodNode mergedMethod, ClassNode mixinNode, ClassNode patchedNode, ClassNode originalNode, LunarVersion version, MappingProvider provider, Map<String, FieldNode> fields, Map<String, MethodNode> methods) {
         mixinNode.methods.stream().filter(md -> md.name.equals(strippedMethodName(mergedMethod.name))).findFirst().ifPresent(modifyVarMethod -> {
             if (modifyVarMethod.visibleAnnotations == null) {
-                throw new RuntimeException(String.format("@ModifyVariable missing from %s.%s", mixinNode.name, modifyVarMethod.name));
+                //throw new RuntimeException(String.format("@ModifyVariable missing from %s.%s", mixinNode.name, modifyVarMethod.name));
             }
             clone(mergedMethod.instructions, modifyVarMethod.instructions);
         });
